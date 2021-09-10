911208a173fccc132caa539d45391e0e42ae3388
Juan Pablo Cesarini <jcesarini@matelab.com.ar>
2021-09-10 19:08:39 -0300

    primer componente

--- cursoudemy/src/App.js
+++ cursoudemy/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import PrimerComponente from './Components/PrimerComponente';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        <>
+        <PrimerComponente/>
+        </>
+    );
 }
 
 export default App;
---
+++ cursoudemy/src/Components/PrimerComponente.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PrimerComponente extends Component {
+    render() {
+        return ( 
+            <div>
+                <h1>Hola Aca el formulario</h1>
+            </div>
+        )
+    }
+}
